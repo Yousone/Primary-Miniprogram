4a35513b03b409114f1abc454b097bb7b30d0581
3396815007 <2957593192@qq.com>
2022-05-21 13:34:06 +0800

    aah

---
+++ pages/logs/logs.js
@@ -0,0 +1,20 @@
+// logs.js
+const util = require('../../utils/util.js')
+Page({
+  data: {
+    value: '',
+  },
+  onChange(e) {
+    this.setData({
+      value: e.detail,
+    });
+  },
+  onSearch() {
+    Toast('搜索' + this.data.value);
+  },
+  onClick() {
+    Toast('搜索' + this.data.value);
+  },
+});
+
+
